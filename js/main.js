fdb85b448e2d53e2514d038a13a9f33d1147b28a
Arjay Osma <arjay.osma@gmail.com>
2019-04-27 19:21:38 +0800

    Update contents

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-(function ($) {
+(function($) {
 
     "use strict";
     $(".carousel-inner .item:first-child").addClass("active");
     /* Mobile menu click then remove
     ==========================*/
-    $(".mainmenu-area #primary_menu li a").on("click", function () {
+    $(".mainmenu-area #primary_menu li a").on("click", function() {
         $(".navbar-collapse").removeClass("in");
     });
     /* Scroll to top
     ===================*/
-    $.scrollUp({
-        scrollText: '<i class="lnr lnr-arrow-up"></i>',
-        easingType: 'linear',
-        scrollSpeed: 900,
-        animation: 'fade'
-    });
+    // $.scrollUp({
+    //     scrollText: '<i class="lnr lnr-arrow-up"></i>',
+    //     easingType: 'linear',
+    //     scrollSpeed: 900,
+    //     animation: 'fade'
+    // });
     /* testimonials Slider Active
     =============================*/
     $('.gallery-slide').owlCarousel({
@@ -70,7 +70,7 @@
     -----------------------------*/
     $('#mc-form').ajaxChimp({
         url: 'https://quomodosoft.us14.list-manage.com/subscribe/post?u=b2a3f199e321346f8785d48fb&amp;id=d0323b0697', //Set Your Mailchamp URL
-        callback: function (resp) {
+        callback: function(resp) {
             if (resp.result === 'success') {
                 $('.subscrie-form, .join-button').fadeOut();
                 $('body').css('overflow-y', 'scroll');
@@ -83,7 +83,7 @@
         // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
-        .click(function (event) {
+        .click(function(event) {
             // On-page links
             if (
                 location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
@@ -98,7 +98,7 @@
                     event.preventDefault();
                     $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 1000, function () {
+                    }, 1000, function() {
                         // Callback after animation
                         // Must change focus!
                         var $target = $(target);
@@ -116,7 +116,7 @@
     /*--------------------
        MAGNIFIC POPUP JS
        ----------------------*/
-    var magnifPopup = function () {
+    var magnifPopup = function() {
         $('.popup').magnificPopup({
             type: 'iframe',
             removalDelay: 300,
@@ -133,7 +133,7 @@
                 // The "opener" function should return the element from which popup will be zoomed in
                 // and to which popup will be scaled down
                 // By defailt it looks for an image tag:
-                opener: function (openerElement) {
+                opener: function(openerElement) {
                     // openerElement is the element on which popup was initialized, in this case its <a> tag
                     // you don't need to add "opener" option if this code matches your needs, it's defailt one.
                     return openerElement.is('img') ? openerElement : openerElement.find('img');
@@ -145,7 +145,7 @@
     magnifPopup();
     /* Preloader Js
     ===================*/
-    $(window).on("load", function () {
+    $(window).on("load", function() {
         $('.preloader').fadeOut(500);
         /*WoW js Active
         =================*/
